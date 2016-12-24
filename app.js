a5f03c1074338ef2527e69e1c1b3c098b71db5f1
Rajesh Pillai <pillai.rajesh@gmail.com>
2016-12-24 09:55:23 +0530

    Refactored and moved code to app folder

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
 var express = require('express'),
-    app = express(),
-    http = require('http').Server(app),
-    io = require('socket.io')(http);
+  app = express(),
+  http = require('http').Server(app),
+  io = require('socket.io')(http);
 
 
-app.use("/assets",express.static(__dirname + '/assets'));
+app.use("/assets", express.static(__dirname + '/assets'));
+app.use("/app", express.static(__dirname + '/app'));
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
 })
 
 http.listen(9999, function () {
-    console.log(__dirname);
-    console.log("Server running on 9999...");
+  console.log(__dirname);
+  console.log("Server running on 9999...");
 })
 
 
 // Listen for incoming connections from clients
 io.on('connection', function (socket) {
 
-	// Start listening for mouse move events
-	socket.on('mousemove', function (data) {
+  // Start listening for mouse move events
+  socket.on('mousemove', function (data) {
 
-		// This line sends the event (broadcasts it)
-		// to everyone except the originating client.
-		socket.broadcast.emit('moving', data);
-	});
+    // This line sends the event (broadcasts it)
+    // to everyone except the originating client.
+    socket.broadcast.emit('moving', data);
+  });
 });
